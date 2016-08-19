bd70affb0f54cc456267d21cea19a699c1d7feb2
Timothy Herold <heroldt@gmail.com>
2016-08-19 14:53:00 -0700

    Significant refactor of scripts.js. Pulls repeated calls to enable/disable ui elements into a separate function.

--- js/scripts.js
+++ js/scripts.js
@@ -22,19 +22,31 @@ var pingPong = function(number) {
 
 // Front end logic
 $(document).ready(function() {
+  var toggleScreen = function() {
+    $(".counting").slideToggle();
+    $(".done").slideToggle();
+    $("button#stop").toggle();
+    $("button#top").toggle();
+    if ($("button#submit").prop("disabled")) {
+      $("button#submit").prop("disabled", false);
+    } else {
+      $("button#submit").prop("disabled", true);
+    }
+    if ($("input#input-number").prop("disabled")) {
+      $("input#input-number").prop("disabled", false);
+    } else {
+      $("input#input-number").prop("disabled", true);
+    }
+  };
+
   $("button#top").on("click", function(){
     $("body").animate({ scrollTop: 0 }, 200);
   });
 
   $("button#stop").on("click", function(){
-    $(".counting, .done, p.result").clearQueue();
-    $(".counting, .done, p.result").stop();
-    $(".counting").slideUp();
-    $(".done").slideDown();
-    $("button#stop").hide();
-    $("button#top").show();
-    $("button#submit").prop("disabled", false);
-    $("input#input-number").prop("disabled", false);
+    $(".counting, .done, p.result").clearQueue().stop();
+    $(".done").text("Counting canceled! Shall we play again?");
+    toggleScreen();
   });
 
   $("form#input-form").submit(function(event) {
@@ -46,34 +58,35 @@ $(document).ready(function() {
       var delay = 400;
       $("div.results").children().remove();
       $("body").animate({ scrollTop: $(".results").offset().top }, 200);
-      $(".done").slideUp();
-      $(".counting").slideDown();
-      $("button#top").hide();
-      $("button#stop").show();
-      $("input#input-number").prop("disabled", true);
-      $("button#submit").prop("disabled", true);
+      toggleScreen();
       outputs.forEach(function(output, i) {
         if (output === "ping" || output === "pong") {
-          $("div.results").prepend("<p class='result'><span class='" + output + "'>" + output + "</span></p>");
-          $("p.result:nth-child(1)").delay(delay * i).animate({height:'toggle'});
-          $("p.result:nth-child(1)").children().delay(delay * i).animate({left:'0px', right: '0px'});
+          $("div.results").prepend(
+            "<p class='result'>" +
+              "<span class='" + output + "'>" + output + "</span>" +
+            "</p>");
+          $("p.result:nth-child(1)")
+            .delay(delay * i).animate({height:'toggle'})
+            .children().delay(delay * i).animate({left:'0px', right: '0px'});
         } else if (output === "ping-pong") {
-          $("div.results").prepend("<p class=result><span class='ping'>ping</span> - <span class='pong'>pong</span></p>");
-          $("p.result:nth-child(1)").delay(delay * i).animate({height:'toggle'});
-          $("p.result:nth-child(1)").children().each(function() {
-            $(this).delay(delay * i).animate({left:'0px', right: '0px'});
-          });
+          $("div.results").prepend(
+            "<p class=result>" +
+              "<span class='ping'>ping</span> - <span class='pong'>pong</span>" +
+            "</p>");
+          $("p.result:nth-child(1)")
+            .delay(delay * i).animate({height:'toggle'})
+            .children().each(function() {
+              $(this).delay(delay * i).animate({left:'0px', right: '0px'});
+            });
         } else {
           $("div.results").prepend("<p class=result>" + output + "</p>");
           $("p.result:nth-child(1)").delay(delay * i).slideDown();
         }
       });
-      $(".counting").delay(delay * outputs.length).slideUp(function() {
-        $(".done").slideDown();
-        $("input#input-number").prop("disabled", false);
-        $("button#stop").hide();
-        $("button#top").show();
-        $("button#submit").prop("disabled", false);
+
+      $("body").delay(delay * outputs.length).show(function() {
+        $(".done").text("All done! Shall we play again?");
+        toggleScreen();
       });
     }
     event.preventDefault();
