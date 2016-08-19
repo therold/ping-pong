65507c9005e7d3166b53d8132788c25fb39f96fe
Timothy Herold <heroldt@gmail.com>
2016-08-19 14:10:09 -0700

    Add javascript to disable text input and submit button while animation is playing

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business logic
 var pingPong = function(number) {
   if (!number || number < 1)  {
     return false
@@ -18,6 +19,8 @@ var pingPong = function(number) {
   }
 }
 
+
+// Front end logic
 $(document).ready(function() {
   $("button#top").on("click", function(){
     $("body").animate({ scrollTop: 0 }, 200);
@@ -30,6 +33,8 @@ $(document).ready(function() {
     $(".done").slideDown();
     $("button#stop").hide();
     $("button#top").show();
+    $("button#submit").prop("disabled", false);
+    $("input#input-number").prop("disabled", false);
   });
 
   $("form#input-form").submit(function(event) {
@@ -43,8 +48,10 @@ $(document).ready(function() {
       $("body").animate({ scrollTop: $(".results").offset().top }, 200);
       $(".done").slideUp();
       $(".counting").slideDown();
-      $("button#top").hide(function() { return true; });
-      $("button#stop").show(function() { return true; });
+      $("button#top").hide();
+      $("button#stop").show();
+      $("input#input-number").prop("disabled", true);
+      $("button#submit").prop("disabled", true);
       outputs.forEach(function(output, i) {
         if (output === "ping" || output === "pong") {
           $("div.results").promise().done(function() {
@@ -52,7 +59,6 @@ $(document).ready(function() {
             $("p.result:nth-child(1)").delay(delay * i).animate({height:'toggle'});
             $("p.result:nth-child(1)").children().delay(delay * i).animate({left:'0px', right: '0px'});
           });
-
         } else if (output === "ping-pong") {
           $("div.results").prepend("<p class=result><span class='ping'>ping</span> - <span class='pong'>pong</span></p>");
           $("p.result:nth-child(1)").delay(delay * i).animate({height:'toggle'});
@@ -66,16 +72,13 @@ $(document).ready(function() {
           });
         }
       });
-      $("button#stop").delay(delay * outputs.length).hide(function() {
-        return true;
-      });
-      $("button#top").delay(delay * outputs.length).show(function() {
-        return true;
-      });
       $(".counting").delay(delay * outputs.length).slideUp(function() {
         $(".done").slideDown();
+        $("input#input-number").prop("disabled", false);
+        $("button#stop").hide();
+        $("button#top").show();
+        $("button#submit").prop("disabled", false);
       });
-
     }
     event.preventDefault();
   });
