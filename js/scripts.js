2b891979fbe1c8249ff473c2a93104cdf057b209
Timothy Herold <heroldt@gmail.com>
2016-08-19 13:58:15 -0700

    Add button to stop animations

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,15 @@ $(document).ready(function() {
     $("body").animate({ scrollTop: 0 }, 200);
   });
 
+  $("button#stop").on("click", function(){
+    $(".counting, .done, p.result").clearQueue();
+    $(".counting, .done, p.result").stop();
+    $(".counting").slideUp();
+    $(".done").slideDown();
+    $("button#stop").hide();
+    $("button#top").show();
+  });
+
   $("form#input-form").submit(function(event) {
     var userInput = parseInt($("input#input-number").val());
     var outputs = pingPong(userInput);
@@ -31,15 +40,19 @@ $(document).ready(function() {
     if (!outputs) {
       alert("Please enter a number!");
     } else {
-      // $(".results").get(0).scrollIntoView();
       $("body").animate({ scrollTop: $(".results").offset().top }, 200);
       $(".done").slideUp();
       $(".counting").slideDown();
+      $("button#top").hide(function() { return true; });
+      $("button#stop").show(function() { return true; });
       outputs.forEach(function(output, i) {
         if (output === "ping" || output === "pong") {
-          $("div.results").prepend("<p class='result'><span class='" + output + "'>" + output + "</span></p>");
-          $("p.result:nth-child(1)").delay(delay * i).animate({height:'toggle'});
-          $("p.result:nth-child(1)").children().delay(delay * i).animate({left:'0px', right: '0px'});
+          $("div.results").promise().done(function() {
+            $(this).prepend("<p class='result'><span class='" + output + "'>" + output + "</span></p>");
+            $("p.result:nth-child(1)").delay(delay * i).animate({height:'toggle'});
+            $("p.result:nth-child(1)").children().delay(delay * i).animate({left:'0px', right: '0px'});
+          });
+
         } else if (output === "ping-pong") {
           $("div.results").prepend("<p class=result><span class='ping'>ping</span> - <span class='pong'>pong</span></p>");
           $("p.result:nth-child(1)").delay(delay * i).animate({height:'toggle'});
@@ -47,13 +60,22 @@ $(document).ready(function() {
             $(this).delay(delay * i).animate({left:'0px', right: '0px'});
           });
         } else {
-          $("div.results").prepend("<p class=result>" + output + "</p>");
-          $("p.result:nth-child(1)").delay(delay * i).slideDown();
+          $("div.results").promise().done(function() {
+            $(this).prepend("<p class=result>" + output + "</p>");
+            $("p.result:nth-child(1)").delay(delay * i).slideDown();
+          });
         }
+      });
+      $("button#stop").delay(delay * outputs.length).hide(function() {
+        return true;
+      });
+      $("button#top").delay(delay * outputs.length).show(function() {
+        return true;
       });
       $(".counting").delay(delay * outputs.length).slideUp(function() {
         $(".done").slideDown();
       });
+
     }
     event.preventDefault();
   });
